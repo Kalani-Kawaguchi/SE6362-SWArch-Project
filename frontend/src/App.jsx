214b38ba311ce389a331a3eb8541ac871d0cde5e
Kalani-Kawaguchi <kkawa0442@gmail.com>
2026-09-10 20:04:34 -0500

    Initial Setup
    Setup Frontend with React, Node.js, and Parcel

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <main>
+      <h1>Software Architecture Project</h1>
+      <h2>Group: Kalani Kawaguchi, Zhi Li, Jonathan Loper, Mitchell Vu</h2>
+      <a href="PreliminaryProjectPlan.pdf">Preliminary Project Plan</a>
+    </main>
+  );
+}
