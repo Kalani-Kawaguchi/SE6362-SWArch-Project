5fd1797cbbbea422206a29cf712e1aa8a46eade4
Kalani-Kawaguchi <kkawa0442@gmail.com>
2026-09-12 15:49:44 -0500

    Spring Boot Setup
    Setup the backend using Spring Boot.
    Project: Maven
    Java
    Spring Boot version 4.1.1
    
    Group: com.se6362
    Artifact: backend
    Package Name: com.se6362.kwic
    Jar with Java version 21

---
+++ backend/src/main/java/com/se6362/kwic/controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.se6362.kwic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController 
+@RequestMapping ("/api")
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "KWIC backend is running!";
+    }
+}
---
+++ backend/src/test/java/com/se6362/kwic/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se6362.kwic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
